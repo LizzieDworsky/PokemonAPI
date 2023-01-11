750b8e398d59b8315194699f6d4c512865c738a3
LizzieDworsky <amydworsky15@gmail.com>
2023-01-10 18:26:04 -0700

    added url path for pokemon app and started working on CRUD

---
+++ pokemon/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.all_pokemon),
+    path('<int:pk>/', views.pokemon_by_id)
+]
--- pokemon/views.py
+++ pokemon/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def all_pokemon():
+    pass
+
+def pokemon_by_id():
+    pass
