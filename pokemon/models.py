43680353176c6df740f246b5cc7f840f9fd504cb
LizzieDworsky <amydworsky15@gmail.com>
2023-01-10 17:13:15 -0700

    created models, serializers, registered models with admin site and added apps to installed apps for the project

--- pokemon/models.py
+++ pokemon/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from type.models import Type
+from attack.models import Attack
 
 # Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=255)
+    health = models.IntegerField()
+    poke_type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    attacks = models.ManyToManyField(Attack)
--- pokemon_api/settings.py
+++ pokemon_api/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'pokemon',
+    'attack',
+    'type'
 ]
 
 MIDDLEWARE = [
